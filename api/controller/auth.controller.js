4de1eade48170038f673a82f840f6278b6b09f9b
kaleb <leolittleprogrammer@gmail.com>
2024-01-27 19:58:48 +0300

    create and add a middle ware and a function to handle errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,24 +1,27 @@
 const User = require('../models/user.model.js')
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const errorHandler = require('../utils/error.js');
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({message: 'All fields are required.'})
+        next(errorHandler(400, 'All fields are required.'));
     }
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword
-    });
-    try{
-        await newUser.save();
-        res.json({message: 'Signup successful'})
-    } catch(err) {
-        res.status(500).json({ message: err.message})
+    else {
+        const hashedPassword = bcryptjs.hashSync(password, 10)
+        
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+        try{
+            await newUser.save();
+            res.json({message: 'Signup successful'})
+        } catch(error) {
+            next(error)
+        }
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -13,7 +13,15 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
-
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
 
 app.listen(3000, ()=>{
     console.log('Server is runnig on port 3000.')
