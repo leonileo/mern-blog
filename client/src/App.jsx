3beb9ab32b4da7f7ce722c1800d33665cf26eb45
kaleb <leolittleprogrammer@gmail.com>
2024-02-03 22:59:53 +0300

    create and add the footer component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import About from './pages/About'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path='/dashboard' element={<Dashboard/>} />
         <Route path='/projects' element={<Projects/>} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Footer } from 'flowbite-react'
+import { Link } from 'react-router-dom'
+import {BsFacebook, BsInstagram, BsTwitterX, BsGithub, BsDribbble} from 'react-icons/bs'
+
+const FooterCom = () => {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+            <div className='mt-5'>
+            <Link to='/' className='self-center whitespace-nowrap text-md sm:text-xl font-semibold dark:text-white'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Mern</span>Blog
+            </Link>
+            </div>
+            <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+                <div>
+                    <Footer.Title title='About'/>
+                    <Footer.LinkGroup col>
+                        <Footer.Link href='/' rel='noopener noreferrer'>Home</Footer.Link>
+                    </Footer.LinkGroup>
+                </div>
+                <div>
+                    <Footer.Title title='Follow us'/>
+                    <Footer.LinkGroup col>
+                        <Footer.Link href='https://github.com' target='_blank' rel='noopener noreferrer'>Github</Footer.Link>
+                    </Footer.LinkGroup>
+                    <Footer.LinkGroup col>
+                        <Footer.Link href='https://youtube.com' target='_blank' rel='noopener noreferrer'>Youtube</Footer.Link>
+                    </Footer.LinkGroup>
+                    <Footer.LinkGroup col>
+                        <Footer.Link href='https://discord.com' target='_blank' rel='noopener noreferrer'>Discord</Footer.Link>
+                    </Footer.LinkGroup>
+                </div>
+                <div>
+                    <Footer.Title title='Legal'/>
+                    <Footer.LinkGroup col>
+                        <Footer.Link href='/' rel='noopener noreferrer'>Privacy Policy</Footer.Link>
+                        <Footer.Link href='/' rel='noopener noreferrer'>Terms & Conditions</Footer.Link>
+                    </Footer.LinkGroup>
+                </div>
+            </div>
+        </div>
+        <Footer.Divider/>
+        <div className='sm:justify-between sm:flex w-full sm:items-center'>
+            <Footer.Copyright href='#' by='Mern blog' year={new Date().getFullYear()}/>
+            <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
+                <Footer.Icon href='#' icon={BsFacebook}/>
+                <Footer.Icon href='#' icon={BsInstagram}/>
+                <Footer.Icon href='#' icon={BsTwitterX}/>
+                <Footer.Icon href='#' icon={BsGithub}/>
+                <Footer.Icon href='#' icon={BsDribbble}/>
+            </div>
+        </div>
+      </div>
+    </Footer>
+  )
+}
+
+export default FooterCom
